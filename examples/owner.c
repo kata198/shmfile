8b4a3118d1c10315cc8ac68625307352907024ee
Tim Savannah <kata198@gmail.com>
2018-07-24 23:19:27 -0400

    Use unsigned char type for version in examples/owner.c

--- examples/owner.c
+++ examples/owner.c
@@ -43,14 +43,14 @@ int main(int argc, char* argv[])
 
     char *streamName;
     int sleepTime;
-    char vmajor, vminor, vpatchlevel;
+    unsigned char vmajor, vminor, vpatchlevel;
     const char* vextra;
 
     OwnerGuestData data = { 0 };
 
     shmfile_get_version(&vmajor, &vminor, &vpatchlevel, &vextra);
 
-    printf("shmfile version: %hhd.%hhd.%hhd%s\n", vmajor, vminor, vpatchlevel, vextra);
+    printf("shmfile version: %hhu.%hhu.%hhu%s\n", vmajor, vminor, vpatchlevel, vextra);
 
     if ( argc == 1 )
     {
